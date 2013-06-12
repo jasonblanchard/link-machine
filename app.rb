310278418a72ab0cf7a5d54f41388a3e473464b6
jasonblanchard <jas.blanchard@gmail.com>
2013-06-11 21:24:15 -0400

    App working

--- app.rb
+++ app.rb
@@ -5,10 +5,27 @@ Mongoid.load!("config/mongoid.yml")
 
 class Link
     include Mongoid::Document
+    include Mongoid::Timestamps
 
     field :url, :type => String
 end
 
 get '/hi' do
     "hello, world"
+end
+
+get '/new' do
+    @current_link = Link.last
+    erb :new
+end
+
+get '/history' do
+    @links = Link.desc(:created_at).limit(20).map(&:url)
+    erb :history
+end
+
+post '/create' do
+    link = params[:link]
+    Link.create(:url => link)
+    redirect to('/new')
 end
